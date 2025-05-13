0c7a55394a63fdc3842a213e04011d5018243233
lutfiusluer <byusluerdev@gmail.com>
2025-05-13 17:32:43 +0300

    fix dockerize for containers

--- hotel-service/src/main/java/com/byusluer/hotelservice/controller/HotelController.java
+++ hotel-service/src/main/java/com/byusluer/hotelservice/controller/HotelController.java
@@ -5,7 +5,6 @@ import com.byusluer.hotelservice.service.HotelService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,13 +18,7 @@ import java.util.List;
 public class HotelController {
     private final HotelService hotelService;
 
-    @Value("${hotel.welcome-message}")
-    private String welcomeMessage;
 
-    @GetMapping("/welcome")
-    public String getWelcomeMessage() {
-        return welcomeMessage;
-    }
 
     @PostMapping
     public ResponseEntity<HotelDto> createHotel(@RequestBody @Valid HotelDto hotelDto) {
